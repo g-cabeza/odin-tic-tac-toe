30c46e85baa3df24dab92d765389989185423add
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-09-15 14:14:37 -0300

    Assignment complete. Dors Coding School video used as supplement.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message;
+
+    }
+
+    return{
+        renderMessage,
+    }
+})();
+
+
 const Gameboard = (()=>{
     let gameboard =["","","","","","","","",""]
     
@@ -51,6 +63,10 @@ const Game = (() => {
     }
     
     const handleClick = (event) => {
+        if (gameOver) {
+            return;
+        }
+
         let index = parseInt(event.target.id.split ("-")[1]);
         
 
@@ -58,6 +74,17 @@ const Game = (() => {
             return;
 
         Gameboard.update(index,players[currentPlayerIndex].mark);
+        
+
+        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+            gameOver = true;
+           displayController.renderMessage (`${players[currentPlayerIndex].name} wins!`);
+
+        } else if (checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+           displayController.renderMessage("It's a tie!");
+        }
+
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
@@ -66,6 +93,8 @@ const Game = (() => {
             Gameboard.update(i,"");
         }
         Gameboard.render();
+        document.querySelector("#message").innerHTML = "";
+        gameOver = false;
     }
 
     return {
@@ -74,6 +103,32 @@ const Game = (() => {
         restart
     }
 })();
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a,b,c] = winningCombinations [i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+function checkForTie (board) {
+    return board.every(cell => cell !== "");
+}
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
